7912827b2d760eaaafa0fde971ad8c779736b756
Lucius Vinicius <luciusviniciusf@ua.pt>
2021-11-17 13:49:26 +0000

    models on admin

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from wiki.models import Category, Article, Section, Subsection
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Article)
+admin.site.register(Section)
+admin.site.register(Subsection)
