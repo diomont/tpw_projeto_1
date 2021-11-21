dd0e17ceb342103d5820b4a90998bfb176aa1637
Diogo Monteiro <56603629+CrustyMarsh@users.noreply.github.com>
2021-11-21 16:58:21 +0000

    some improvements to form

--- wiki/forms.py
+++ wiki/forms.py
@@ -24,16 +24,6 @@ class SideCardWidget(forms.Widget):
         return json.loads(value)
 
 
-class SectionWidget(forms.Widget):
-    template_name = "widgets/sectionbody.html"
-
-    def __init__(self, attrs=None):
-        super().__init__(attrs)
-
-    def format_value(self, value):
-        return json.loads(value)
-
-
 # https://docs.djangoproject.com/en/3.2/topics/forms/modelforms/
 class ArticleForm(forms.ModelForm):
     class Meta:
@@ -52,13 +42,9 @@ class ArticleForm(forms.ModelForm):
         self.fields['categories'].widget.attrs.update({'class': 'form-control w-50'})
 
 
-
-# TODO: Custom class to transform JSON into form, and correctly save it
-# TODO: Show sections of article (from view most likely as a collection of forms)
-
 # https://docs.djangoproject.com/en/3.2/ref/forms/fields/#creating-custom-fields
-SectionFormSet = forms.modelformset_factory(Section, fields=("title", "text", "image"),
-                                            extra=0, widgets={
+SectionFormSet = forms.modelformset_factory(Section, fields=("title", "text", "image"), extra=0,
+    widgets={
         "image": ImagePicker(),
         "title": forms.TextInput(attrs={
             "class": "form-control"
