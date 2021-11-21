5ef1f7b4520c9073b85bf04cb8b4ed1f52160f88
Lucius Vinicius <luciusviniciusf@ua.pt>
2021-11-21 15:04:47 +0000

    edit css usable

--- wiki/forms.py
+++ wiki/forms.py
@@ -33,6 +33,14 @@ class ArticleForm(forms.ModelForm):
             "side_card": SideCardWidget()
         }
 
+    def __init__(self, *args, **kwargs):
+        super(ArticleForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Title'})
+        self.fields['short_description'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Short Description'})
+        self.fields['main_text'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Main Text'})
+        self.fields['categories'].widget.attrs.update({'class': 'form-control w-50'})
+
+
 
 # TODO: Custom class to transform JSON into form, and correctly save it
 # TODO: Show sections of article (from view most likely as a collection of forms)
