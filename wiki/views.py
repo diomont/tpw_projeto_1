dc8a9100f74b1d53c771f86841aa75c849178470
Diogo Monteiro <56603629+CrustyMarsh@users.noreply.github.com>
2021-11-22 23:47:29 +0000

    article user fields, but working this time

--- wiki/views.py
+++ wiki/views.py
@@ -98,14 +98,14 @@ def article_save(request, art_id=None):
         if art_id is not None:
             try:
                 a = Article.objects.get(id=art_id)
+                a.last_modified_by_user = request.user
                 form = ArticleForm(post, request.FILES, instance=a)
-                form.last_modified_by_user = request.user
             except Exception:
                 form = ArticleForm(post, request.FILES)
-                form.created_by_user = request.user
+                form.instance.created_by_user = request.user
         else:
             form = ArticleForm(post, request.FILES)
-            form.created_by_user = request.user
+            form.instance.created_by_user = request.user
 
         if form.is_valid():
             inst = form.save()
